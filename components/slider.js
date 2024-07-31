d753110cc564d59ef9430e099f394dedeb588a55
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-07-31 12:13:51 +0800

    slider changing

--- components/slider.js
+++ components/slider.js
@@ -7,17 +7,7 @@ import { IoIosArrowDroprightCircle } from "react-icons/io";
 
 export function Slider() {
 const [news, setNews] = useState([])
-const [indx, setIndx] = useState([])
-
-function slideLeft(item, index){
-   news[index-1].social_image.style.display = "block"
-   news[index].social_image.style.display = "hidden"
-}
-
-function slideRight(social_image, index){
-    news[index +1].social_image.style.display  = "block"
-    news[index].social_image.style.display  = "hidden"
-}
+const [indexnumber, setIndexnumber] = useState(0)
 
 useEffect(()=> {
     fetch(`https://dev.to/api/articles?username=ben&per_page=9`)
@@ -25,18 +15,28 @@ useEffect(()=> {
     .then((data) => {setNews(data)});},
     [])
 return (
-    <div>
-        <div>{news.map((item, index) => (
+    <div className="w-full xl:max-w-screen-xl xl:-m-auto">
+        <div className="m-auto">{news.map((item, index) => (
                <div>
                      <div key={item.id}>
-                        <Image src={item.social_image} width={600} height={400} alt="Image" className={`${index==0? "block":"hidden"}`}/>
-                    </div>
-                    <div className="flex gap-2">
-                        <IoIosArrowDropleftCircle onClick={slideLeft(social_image,index)} className="w-20 h-20" />
-                        <IoIosArrowDroprightCircle onClick={slideRight(social_image,index)} className="w-20 h-20"/>
+                        <Image src={item.social_image} width={600} height={400} alt="Image" className={`${index=== indexnumber? "block":"hidden"}`}/>
                     </div>
                </div>
             ))}
+            <div className="flex gap-2">
+                        <IoIosArrowDropleftCircle onClick={()=> {
+                            setIndexnumber(indexnumber-1)
+                            if(indexnumber === 0) {
+                                setIndexnumber (news.length)
+                            }
+                        }} className="w-20 h-20" />
+                        <IoIosArrowDroprightCircle onClick={()=> {
+                            setIndexnumber(indexnumber+1)
+                            if(indexnumber === news.length) {
+                                setIndexnumber (0)
+                            }
+                        }} className="w-20 h-20"/>
+            </div>
         </div>
     </div>
 )

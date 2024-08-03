d864abf95042585640615262400f49bb5084b59e
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-08-03 21:53:09 +0800

    trending part adding

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { Post } from "@/components/post";
 import { Swipersnew } from "@/components/swiper";
+import { Trending } from "@/components/trending";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main className=" p-8">
       <Swipersnew/>
+      <Trending/>
       <Post/>
     </main>
   );

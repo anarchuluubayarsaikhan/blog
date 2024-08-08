10beb5b07cf9f797e3260c064350b01326fe6e71
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-08-08 16:59:01 +0800

    search  button size changing

--- components/header.js
+++ components/header.js
@@ -11,12 +11,12 @@ export function Header() {
       <Nav />
       <form action="/search">
           <label className="input input-bordered flex items-center gap-2" >
-            <input type="text" className="grow" placeholder="Search" name="q" />
+            <input type="text" className="grow" placeholder="Search" name="q " />
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 16 16"
               fill="currentColor"
-              className="h-4 w-4 opacity-70">
+              className="xl:h-4 xl:w-4 opacity-70 h-2 w-2">
               <path
                 fillRule="evenodd"
                 d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"

a93acf884768e15dfe5b34323ab39e08bf8f413c
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-08-09 16:48:17 +0800

    slug css changing

--- components/trendings.js
+++ components/trendings.js
@@ -15,7 +15,7 @@ export function Trendings() {
             <p className="text-title-color font-bold text-2xl">Trending</p>
             <div className="w-full max-h-[320px] rounded-xl flex flex-col md:flex-row md:gap-5 xl:max-w-screen-xl md:max-w-screen-xl overflow-scroll">
                 {trendings.map((trending) => (
-                    <Link href={trending.path} className="relative">
+                    <Link href={trending.path} className="relative h-full">
                     <Image src={trending.social_image} alt="Image of an article" className="aspect-video w-full h-full md:min-w-[293px] md:min-h-[320px]" width={293} height={320}/>
                     <div className="flex flex-col gap-4 xl:max-h-[76px] absolute bottom-7 left-7">
                         <div className="badge badge-primary p-3">{trending.tag_list[0]}</div>

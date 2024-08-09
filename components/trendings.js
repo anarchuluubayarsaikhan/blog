57b251644728ac357ca01a0510a7f94603e9576e
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-08-09 17:12:48 +0800

    trending gap adding

--- components/trendings.js
+++ components/trendings.js
@@ -13,7 +13,7 @@ export function Trendings() {
     return (
        <div className="m-auto xl:max-w-screen-xl md:max-w-screen-xl flex flex-col gap-7 pt-[30px] font-sans xl:pt-[100px]">
             <p className="text-title-color font-bold text-2xl">Trending</p>
-            <div className="w-full max-h-[320px] rounded-xl flex flex-col md:flex-row md:gap-5 xl:max-w-screen-xl md:max-w-screen-xl overflow-scroll">
+            <div className="w-full max-h-[320px] rounded-xl flex flex-col md:flex-row md:gap-5 xl:max-w-screen-xl md:max-w-screen-xl overflow-scroll gap-5">
                 {trendings.map((trending) => (
                     <Link href={trending.path} className="relative h-full">
                     <Image src={trending.social_image} alt="Image of an article" className="aspect-video w-full h-full md:min-w-[293px] md:min-h-[320px]" width={293} height={320}/>
